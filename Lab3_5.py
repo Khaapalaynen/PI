7502bee363aca02a36a78df2a2fce57c71845058
Анастасия <nastyakhaaaa@gmail.com>
2023-10-09 03:25:41 +0500

    u

--- Lab3_5.py
+++ Lab3_5.py
@@ -1,7 +1,10 @@
-i = 1
-while(i!=12):
-    if i%2:
-        print("hello world")
-    else:
-        print("hello")
-    i+=1
+counter = 0
+values = [0,2,4,6,8,10]
+while counter!=10:
+    string = 'hello'
+    memory = ' world'
+    print(string)
+    if counter in values:
+        print(memory)
+    counter+=1 
+    string = memory
